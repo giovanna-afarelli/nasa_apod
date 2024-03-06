c81901cf831ca9bc91617bfb40d69fe741fc955a
Giovanna Afarelli <85625516+giovanna-afarelli@users.noreply.github.com>
2024-03-06 16:20:33 -0300

    feat: add todo comment to splash page

--- lib/app/presentation/pages/splash_screen/splash_screen.dart
+++ lib/app/presentation/pages/splash_screen/splash_screen.dart
@@ -20,6 +20,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   void navigateToHome() async {
+    //TODO: start loading async data in Splash screen
     await Future.delayed(const Duration(seconds: 3));
     if (context.mounted) {
       Navigator.of(context).pushNamed(HomePage.routeName);
